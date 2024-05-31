c1004668a675b20131cdc308a1c5c32775da0d81
panda-pritam <mondalrumki02@gmail.com>
2024-05-31 15:13:44 +0530

    mongo

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,20 @@ const app = require("./app");
 const config = require("./config/config");
 
 // let server=config.env.url;
-let PORT=8082;
+let PORT = config.port;
 // console.log("------------------------------Hello from server---------------------");
 // console.log(config.port)
 // console.log(config.mongoose.url)
 // console.log(config.mongoose.url);
-mongoose.connect("mongodb://127.0.0.1:27017/xflix")
-.then((res)=>console.log("DB Connected "))
-.catch((err)=>{
+mongoose
+  .connect("mongodb://127.0.0.1:27017/xflix")
+  .then((res) => console.log("DB Connected "))
+  .catch((err) => {
     console.log(err);
-    console.log("can't connect to the DB "+err);
-});
+    console.log("can't connect to the DB " + err);
+  });
 
-app.listen(PORT,()=>{
-    console.log(PORT);
-    console.log("Server is runing at "+PORT);
-})
+app.listen(PORT, () => {
+  console.log(PORT);
+  console.log("Server is runing at " + PORT);
+});
